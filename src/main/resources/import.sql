a773445e99725a69734fe6ca7190460450e44414
Matheus Farias <matheusaf.contato@gmail.com>
2026-01-30 15:27:02 -0300

    feat: add `Resource entity`, `ResourceType enum` and `Offer` relationship

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,9 +14,13 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_course(name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa-X324ePVRix7TwjRz8peZ8QtRw3eaMInQg&s');   
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQa-X324ePVRix7TwjRz8peZ8QtRw3eaMInQg&s');
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2026-03-11T17:00:00Z', TIMESTAMP WITH TIME ZONE '2027-03-11T17:00:00Z', 1);
 INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2026-12-11T17:00:00Z', TIMESTAMP WITH TIME ZONE '2027-12-11T17:00:00Z', 1);
+
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha Principal do curso', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 1, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 2, 1);
+INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 0, 1);
 
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Welcome to the platform', CURRENT_TIMESTAMP, false, '/dashboard', 1);
 INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('New course available: Java Basics', CURRENT_TIMESTAMP, false, '/courses/1', 1);
